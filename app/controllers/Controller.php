22257c8c7ee89937a065e3370114459cbd8cb4df
Joachim_Tan <jmtan3@student.apc.edu.ph>
2026-02-13 07:47:19 +0800

    Update Controller.php

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,14 +2,10 @@
 class Controller {
 
     protected function view($view, $data = []) {
-        // Extract data array into variables
         extract($data);
 
-        // Include header
-
-        // Include the main view content
         require "views/$view.php";
 
-        // Include footer
     }
 }
+
